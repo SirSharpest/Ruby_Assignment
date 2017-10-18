3f48b55092410c7ffd8aff2d864cea719710aa92
sirsharpest <nathan1hughes@gmail.com>
2017-10-18 14:27:42 +0100

    Db initially added

--- db/migrate/20171018130850_create_users.rb
+++ db/migrate/20171018130850_create_users.rb
@@ -1,14 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :surname
-      t.string :firstname
+      t.string :surname, null:false
+      t.string :firstname, null:false
       t.string :phone
-      t.integer :grad_year
-      t.boolean :jobs
-      t.string :email
+      t.integer :grad_year, null:false
+      t.boolean :jobs, default:false
+      t.string :email, null:false
 
       t.timestamps
     end
+
+    add_index :users, :surname
+    add_index :users, :email
+    
   end
 end
+
